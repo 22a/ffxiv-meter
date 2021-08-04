0a4a80664fc116c3fb8b2717f8de0fc1eef94fe5
Peter Meehan <meehanp2@tcd.ie>
2021-08-04 03:08:21 +0100

    some more table styles

--- app/components/meter-table.js
+++ app/components/meter-table.js
@@ -1,51 +1,73 @@
 import Component from '@glimmer/component';
 import { action } from '@ember/object';
 import { tracked } from '@glimmer/tracking';
-import humanFormat from 'human-format';
+import approximateNumber from 'approximate-number';
 import {
   combatantData as exampleCombatantData,
   encounterData as exampleEncounterData,
-} from 'ffxiv-meter/lib/example-data-medium';
+} from 'ffxiv-meter/lib/example-data-large';
 
-const humanFormatConfig = {
-  decimals: 1,
-  separator: '',
-};
+const acronymToJobName = (jobNameAcronym) => {
+  let acronymToJobNameMapping = {
+    ARC: 'Archer',
+    GLA: 'Gladiator',
+    LNC: 'Lancer',
+    MRD: 'Marauder',
+    PGL: 'Pugilist',
+    ACN: 'Arcanist',
+    CNJ: 'Conjurer',
+    THM: 'Thaumaturge',
+    ROG: 'Rogue',
+    //
+    BRD: 'Bard',
+    DRG: 'Dragoon',
+    MNK: 'Monk',
+    PLD: 'Paladin',
+    WAR: 'Warrior',
+    BLM: 'Black Mage',
+    WHM: 'White Mage',
+    SCH: 'Scholar',
+    SMN: 'Summoner',
+    NIN: 'Ninja',
+    AST: 'Astrologian',
+    DRK: 'Dark Knight',
+    MCH: 'Machinist',
+    RDM: 'Red Mage',
+    SAM: 'Samurai',
+    BLU: 'Blue Mage',
+    GNB: 'Gunbreaker',
+    DNC: 'Dancer',
+    //
+    LB: 'Limit Break',
+  };
 
-let acronymToJobNameMapping = {
-  BRD: 'Bard',
-  DRG: 'Dragoon',
-  MNK: 'Monk',
-  PLD: 'Paladin',
-  WAR: 'Warrior',
-  BLM: 'Black Mage',
-  WHM: 'White Mage',
-  SCH: 'Scholar',
-  SMN: 'Summoner',
-  NIN: 'Ninja',
-  AST: 'Astrologian',
-  DRK: 'Dark Knight',
-  MCH: 'Machinist',
-  RDM: 'Red Mage',
-  SAM: 'Samurai',
-  BLU: 'Blue Mage',
-  GNB: 'Gunbreaker',
-  DNC: 'Dancer',
+  return acronymToJobNameMapping[jobNameAcronym.toUpperCase()];
 };
 
 let acronymToIconFilepath = (jobNameAcronym) => {
-  let jobName = acronymToJobNameMapping[jobNameAcronym.toUpperCase()];
+  let jobName = acronymToJobName(jobNameAcronym);
   return `/images/job-icons/10/${jobName.replace(' ', '_')}_Icon_10.png`;
 };
 
+const toHumanReadable = (num) => {
+  if (num === '∞') return '';
+  return approximateNumber(Number(num));
+};
+
 let enrichCombatantData = (combatantData) => {
-  return combatantData.map((data) => {
+  let maxDmg = combatantData[0] && combatantData[0].damage;
+  return combatantData.map((data, i) => {
+    console.log(data);
+    if (data.Job === '') data.Job = 'LB';
+    let jobName = acronymToJobName(data.Job);
+    data.jobName = jobName;
+    data.jobNameNoSpace = jobName.replace(' ', '');
     data.jobIconFilepath = acronymToIconFilepath(data.Job);
-    data.humanReadableDamage = humanFormat(
-      Number(data.damage),
-      humanFormatConfig
-    );
-    data.humanReadableDps = humanFormat(Number(data.DPS), humanFormatConfig);
+    data.humanReadableDamage = toHumanReadable(data.damage);
+    data.humanReadableDps = toHumanReadable(data.DPS);
+    data.rank = i + 1;
+    data.percentageOfMaxDmg = Math.floor((data.damage / maxDmg) * 100);
+
     console.log(data);
     return data;
   });
--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -3,11 +3,40 @@ module.exports = {
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
+      zIndex: {
+        '-1': '-1',
+        '-2': '-2',
+      },
       colors: {
+        // FF Logs job colours
         LimitBreak: '#2599be',
         Scholar: '#8657ff',
-        Astrologian: '#ab9400',
-        Ast: '#ab9400',
+        Astrologian: '#ffe74a',
+        Monk: '#d69c00',
+        Pugilist: '#d69c00',
+        Ninja: '#af1964',
+        Rogue: '#af1964',
+        Conjurer: '#fff0dc',
+        WhiteMage: '#fff0dc',
+        DarkKnight: '#d126cc',
+        Gladiator: '#a8d2e6',
+        Paladin: '#a8d2e6',
+        Archer: '#91ba5e',
+        Bard: '#91ba5e',
+        Dragoon: '#4164cd',
+        Lancer: '#4164cd',
+        Marauder: '#cf2621',
+        Warrior: '#cf2621',
+        Machinist: '#6ee1d6',
+        Arcanist: '#2d9b78',
+        Summoner: '#2d9b78',
+        BlackMage: '#a579d6',
+        Thamauturge: '#a579d6',
+        RedMage: '#e87b7b',
+        Samurai: '#e46d04',
+        Dancer: '#e2b0af',
+        Gunbreaker: '#998d50',
+        BlueMage: '#2459ff',
       },
       minWidth: {
         1: '1rem',
