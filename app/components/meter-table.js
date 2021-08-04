8b7b77dddd7b23dbd877c6598b498beece707526
Peter Meehan <meehanp2@tcd.ie>
2021-08-04 20:52:56 +0100

    move the column config up

--- app/components/meter-table.js
+++ app/components/meter-table.js
@@ -7,19 +7,4 @@ export default class MeterTableComponent extends Component {
   get combatants() {
     return this.overlayEventService.combatants;
   }
-
-  columns = [
-    { title: '', value: 'Job', class: 'min-w-5 pr-2 font-mono' },
-    { title: '', value: 'name', class: 'min-w-12 w-full whitespace-nowrap' },
-    {
-      title: 'dps',
-      value: 'humanReadableDps',
-      class: 'text-right min-w-4 pr-2 font-mono',
-    },
-    {
-      title: '',
-      value: 'humanReadableDamage',
-      class: 'text-right min-w-4 pr-1 font-mono text-sm opacity-80',
-    },
-  ];
 }
