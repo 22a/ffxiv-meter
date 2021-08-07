3832787d014b463264f13280ef447a63a6733fdb
Peter Meehan <meehanp2@tcd.ie>
2021-08-07 03:58:02 +0100

    enable embroider build, downgrade ember due to negative interaction with https://github.com/emberjs/ember.js/pull/19540

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -57,5 +57,19 @@ module.exports = function (defaults) {
   // please specify an object with the list of modules as keys
   // along with the exports of each module as its value.
 
-  return app.toTree();
+  if (process.env.NO_EMBROIDER) {
+    return app.toTree();
+  } else {
+    const { Webpack } = require('@embroider/webpack');
+    return require('@embroider/compat').compatBuild(app, Webpack, {
+      staticAddonTestSupportTrees: true,
+      staticAddonTrees: true,
+      staticHelpers: true,
+      staticComponents: true,
+      splitAtRoutes: ['application'], // can also be a RegExp
+      packagerOptions: {
+        // webpackConfig: {},
+      },
+    });
+  }
 };
